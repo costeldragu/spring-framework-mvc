04dd609aa4218bee012cfdd82c367ba4507bc204
Dragu Costel <dragu.costel@yahoo.com>
2018-04-17 00:10:17 +0300

    Added test dependencies.

---
+++ src/test/resources/sql/create-db.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS product_table (
+   `id` INT NOT NULL AUTO_INCREMENT,
+   `category` VARCHAR(50) ,
+   `name` VARCHAR(20),
+   `description` VARCHAR(255) ,
+   `price` DOUBLE
+);
