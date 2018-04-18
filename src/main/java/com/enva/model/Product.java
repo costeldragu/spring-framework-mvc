4e8cb97d74b54a4d2e0e0f36f87870e68e7571a0
DRAGU Costel <costel.dragu@endava.com>
2018-04-18 09:24:50 +0300

    Added test dependencies.

--- src/main/java/com/enva/model/Product.java
+++ src/main/java/com/enva/model/Product.java
@@ -65,6 +65,35 @@ public class Product {
         return this;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Product product = (Product) o;
+
+        if (id != null ? !id.equals(product.id) : product.id != null) return false;
+        if (category != null ? !category.equals(product.category) : product.category != null) return false;
+        if (name != null ? !name.equals(product.name) : product.name != null) return false;
+        if (description != null ? !description.equals(product.description) : product.description != null) return false;
+        return price != null ? price.equals(product.price) : product.price == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (category != null ? category.hashCode() : 0);
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        result = 31 * result + (price != null ? price.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" + "id=" + id + ", category='" + category + '\'' + ", name='" + name + '\'' + ", description='" + description + '\'' + ", price=" + price + '}';
+    }
+
     public static class Builder {
         private Integer id;
         private String category;
--- src/main/java/com/enva/web/controllers/ApiController.java
+++ src/main/java/com/enva/web/controllers/ApiController.java
@@ -3,6 +3,7 @@ package com.enva.web.controllers;
 import com.enva.model.Product;
 import com.enva.repository.ProductRepository;
 import com.enva.service.ProductService;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,12 +27,12 @@ public class ApiController extends BaseController {
         return productService.getAllProducts();
     }
 
-    @RequestMapping(path = "/product/{id}/delete", method = RequestMethod.POST)
+    @RequestMapping(path = "/product/{id}/delete", method = RequestMethod.GET)
     public Boolean showProductConfirmDeletePage(@PathVariable(name = "id") Long id) {
         return productService.deleteProduct(productService.getProduct(id));
     }
 
-    @RequestMapping(path = "/product/update", method = RequestMethod.POST)
+    @RequestMapping(path = "/product/update", method = RequestMethod.POST , consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public Product updateProduct(@RequestBody Product product) {
         return productService.updateProduct(product);
     }
